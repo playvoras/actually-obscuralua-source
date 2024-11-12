9c6ee3def8e1b2cc2369e83672e82d819f6b62fa
playvora <longliator.dev@gmail.com>
2024-11-12 19:50:04 +0700

    Update presets.lua

--- src/presets.lua
+++ src/presets.lua
@@ -8,6 +8,7 @@ return {
             {Name = "Vmify"; Settings = {}},
             {Name = "ConstantArray"; Settings = {Treshold = 1; StringsOnly = true; Shuffle = true; Rotate = true; LocalWrapperTreshold = 0}},
             {Name = "NumbersToExpressions"; Settings = {}},
+            {Name = "StringsToExpressions"; Settings = {}},
             {Name = "WrapInFunction"; Settings = {}}
         }
     }
