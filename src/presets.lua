4c3831b529203fc465f9088ccc96c1a89b0ad47b
playvora <longliator.dev@gmail.com>
2024-11-12 20:14:53 +0700

    Update presets.lua

--- src/presets.lua
+++ src/presets.lua
@@ -1,4 +1,3 @@
--- should be good preset but HIGH FILE SIZE :troll:
 return {
     Strong = {
         LuaVersion = "LuaU"; VarNamePrefix = ""; NameGenerator = "MangledShuffled"; PrettyPrint = false; Seed = 0;
@@ -7,7 +6,21 @@ return {
             {Name = "EncryptStrings"; Settings = {}},
             {Name = "NumbersToExpressions"; Settings = {}},
             {Name = "StringsToExpressions"; Settings = {}},
-            {Name = "Vmify", Settings = { VM = "random" } },
+            {Name = "Vmify"; Settings = { VM = "random" }},
+            {Name = "WrapInFunction"; Settings = {}}
+        }
+    },
+    uhmthisistoohigh = {
+        LuaVersion = "LuaU"; VarNamePrefix = ""; NameGenerator = "MangledShuffled"; PrettyPrint = false; Seed = 0;
+        Steps = {
+            {Name = "PaidAntiTamper"; Settings = {}},
+            {Name = "EncryptStrings"; Settings = {}},
+            {Name = "Vmify"; Settings = { VM = "CompilerD" }},
+            {Name = "NumbersToExpressions"; Settings = {}},
+            {Name = "StringsToExpressions"; Settings = {}},
+            {Name = "Vmify"; Settings = { VM = "random" }},
+            {Name = "NumbersToExpressions"; Settings = {}},
+            {Name = "StringsToExpressions"; Settings = {}},
             {Name = "WrapInFunction"; Settings = {}}
         }
     }
