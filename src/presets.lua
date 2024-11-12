c213abe7ddd58c4aa9f644f4e53f181f6484e5cc
playvora <longliator.dev@gmail.com>
2024-11-12 20:07:55 +0700

    Update presets.lua

--- src/presets.lua
+++ src/presets.lua
@@ -3,13 +3,11 @@ return {
     Strong = {
         LuaVersion = "LuaU"; VarNamePrefix = ""; NameGenerator = "MangledShuffled"; PrettyPrint = false; Seed = 0;
         Steps = {
-            {Name = "Vmify", Settings = { VM = "CompilerD" } },
             {Name = "PaidAntiTamper"; Settings = {}},
-            {Name = "Vmify"; Settings = {}},
             {Name = "EncryptStrings"; Settings = {}},
-            {Name = "ConstantArray"; Settings = {Treshold = 1; StringsOnly = true; Shuffle = true; Rotate = true; LocalWrapperTreshold = 0}},
             {Name = "NumbersToExpressions"; Settings = {}},
             {Name = "StringsToExpressions"; Settings = {}},
+            {Name = "Vmify", Settings = { VM = "random" } },
             {Name = "WrapInFunction"; Settings = {}}
         }
     }
