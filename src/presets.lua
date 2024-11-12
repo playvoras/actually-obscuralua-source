c71e73c187118757f526c9078248bfb2156b39c5
playvora <longliator.dev@gmail.com>
2024-11-12 19:49:30 +0700

    Update presets.lua

--- src/presets.lua
+++ src/presets.lua
@@ -1,28 +1,6 @@
 return {
-    Minify = {
-        LuaVersion = "Lua51"; VarNamePrefix = ""; NameGenerator = "MangledShuffled"; PrettyPrint = false; Seed = 0; Steps = {}
-    };
-    Weak = {
-        LuaVersion = "Lua51"; VarNamePrefix = ""; NameGenerator = "MangledShuffled"; PrettyPrint = false; Seed = 0;
-        Steps = {
-            {Name = "Vmify"; Settings = {}},
-            {Name = "ConstantArray"; Settings = {Treshold = 1; StringsOnly = true}},
-            {Name = "WrapInFunction"; Settings = {}}
-        }
-    };
-    Medium = {
-        LuaVersion = "Lua51"; VarNamePrefix = ""; NameGenerator = "MangledShuffled"; PrettyPrint = false; Seed = 0;
-        Steps = {
-            {Name = "EncryptStrings"; Settings = {}},
-            {Name = "AntiTamper"; Settings = {UseDebug = false}},
-            {Name = "Vmify"; Settings = {}},
-            {Name = "ConstantArray"; Settings = {Treshold = 1; StringsOnly = true; Shuffle = true; Rotate = true; LocalWrapperTreshold = 0}},
-            {Name = "NumbersToExpressions"; Settings = {}},
-            {Name = "WrapInFunction"; Settings = {}}
-        }
-    };
     Strong = {
-        LuaVersion = "Lua51"; VarNamePrefix = ""; NameGenerator = "MangledShuffled"; PrettyPrint = false; Seed = 0;
+        LuaVersion = "LuaU"; VarNamePrefix = ""; NameGenerator = "MangledShuffled"; PrettyPrint = false; Seed = 0;
         Steps = {
             {Name = "Vmify"; Settings = {}},
             {Name = "EncryptStrings"; Settings = {}},
