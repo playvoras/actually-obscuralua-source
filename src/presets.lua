9952ba536ca1b4cf2ec7abaef6ee4a009e4081d2
playvora <longliator.dev@gmail.com>
2024-11-12 20:03:57 +0700

    Update presets.lua

--- src/presets.lua
+++ src/presets.lua
@@ -7,7 +7,6 @@ return {
             {Name = "PaidAntiTamper"; Settings = {}},
             {Name = "Vmify"; Settings = {}},
             {Name = "EncryptStrings"; Settings = {}},
-            {Name = "Vmify"; Settings = {}},
             {Name = "ConstantArray"; Settings = {Treshold = 1; StringsOnly = true; Shuffle = true; Rotate = true; LocalWrapperTreshold = 0}},
             {Name = "NumbersToExpressions"; Settings = {}},
             {Name = "StringsToExpressions"; Settings = {}},
