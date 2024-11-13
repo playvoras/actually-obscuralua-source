8b5c78afc8f29bdc853df64cbd4d08c56626cd10
playvora <longliator.dev@gmail.com>
2024-11-13 19:43:44 +0700

    Update presets.lua

--- src/presets.lua
+++ src/presets.lua
@@ -1,27 +1,54 @@
+local function uuid()
+    return string.gsub('xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx', '[xy]', function(c)
+        return string.format('%x', (c == 'x') and math.random(0, 0xf) or math.random(8, 0xb))
+    end)
+end
+
 return {
-    Strong = {
-        LuaVersion = "LuaU"; VarNamePrefix = ""; NameGenerator = "Mangled"; PrettyPrint = false; Seed = 0;
+    ["Basic"] = {
+        LuaVersion = "LuaU",
+        VarNamePrefix = "",
+        NameGenerator = "MangledShuffled",
+        PrettyPrint = false,
+        Seed = 0,
         Steps = {
-            {Name = "PaidAntiTamper"; Settings = {}},
-            {Name = "EncryptStrings"; Settings = {}},
-            {Name = "NumbersToExpressions"; Settings = {}},
-            {Name = "StringsToExpressions"; Settings = {}},
-            {Name = "Vmify"; Settings = { VM = "random" }},
-            {Name = "WrapInFunction"; Settings = {}}
+            { Name = "Vmify", Settings = { VM = "CompilerA" } },
+            { Name = "EncryptStrings", Settings = {} },
+            { Name = "SplitStrings", Settings = {} },
+            { Name = "WatermarkCheck", Settings = {
+                Content = "This script is safeguarded by the free version of ObscuraLua. Version 1.1.0.",
+                CustomVariable = "__OBSCURALUA__"
+            }},
+            { Name = "FreeAntiTamper", Settings = {} },
+            { Name = "Vmify", Settings = { VM = "CompilerA" } },
+            { Name = "ConstantArray", Settings = {} },
+            { Name = "StringsToExpressions", Settings = {} },
+            { Name = "NumbersToExpressions", Settings = {} },
+            { Name = "WrapInFunction", Settings = {} },
         }
     },
-    uhmthisistoohigh = {
-        LuaVersion = "LuaU"; VarNamePrefix = ""; NameGenerator = "Mangled"; PrettyPrint = false; Seed = 0;
+    ["Strong"] = {
+        LuaVersion = "LuaU",
+        VarNamePrefix = "",
+        NameGenerator = "MangledShuffled",
+        PrettyPrint = false,
+        Seed = 0,
         Steps = {
-            {Name = "ProxifyLocals"; Settings = {}},
-            {Name = "Vmify"; Settings = { VM = "CompilerB" }},
-            {Name = "SplitStrings"; Settings = {}},
-            {Name = "PaidAntiTamper"; Settings = {}},
-            {Name = "EncryptStrings"; Settings = {}},
-            {Name = "NumbersToExpressions"; Settings = {}},
-            {Name = "StringsToExpressions"; Settings = {}},
-            {Name = "Vmify"; Settings = { VM = "random" }},
-            {Name = "WrapInFunction"; Settings = {}}
+            { Name = "Vmify", Settings = { VM = "random" } },
+            { Name = "AddVararg", Settings = {} },
+            { Name = "EncryptStrings", Settings = {} },
+            { Name = "SplitStrings", Settings = {} },
+            { Name = "PaidAntiTamper", Settings = {} },
+            { Name = "WatermarkCheck", Settings = {
+                Content = "This script is safeguarded by the paid version of ObscuraLua. Version 1.1.0.",
+                CustomVariable = "__OBSCURALUA__"
+            }},
+            { Name = "Vmify", Settings = { VM = "random" } },
+            { Name = "ConstantArray", Settings = {} },
+            { Name = "StringsToExpressions", Settings = {} },
+            { Name = "NumbersToExpressions", Settings = {} },
+            { Name = "WrapInFunction", Settings = {} },
+            { Name = "ProxifyLocals", Settings = {} },
         }
     }
 }
